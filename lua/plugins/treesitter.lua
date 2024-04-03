34eb9e4bf66c71c11869fa1163db698c9defae70
Ronald Record <github@ronrecord.com>
2024-04-03 12:16:05 -0700

    Use template treesitter config with additional parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,34 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-    event = "BufReadPre",
-    dependencies = {
-      "hiphish/rainbow-delimiters.nvim",
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "RRethy/nvim-treesitter-textsubjects",
-    },
-    config = function()
-      require("configs.treesitter")
-    end,
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "astro",
+      "css",
+      "gitcommit",
+      "graphql",
+      "html",
+      "javascript",
+      "json",
+      "json5",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "prisma",
+      "query",
+      "regex",
+      "svelte",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "vue",
+      -- add more arguments for adding more treesitter parsers
+    })
+  end,
 }
