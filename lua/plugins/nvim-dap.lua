035cb20cc9f4d2381e07bf66abeadfd2de701df0
Ronald Record <github@ronrecord.com>
2024-04-03 13:43:15 -0700

    Customize autopairs config, silence dapui toggle

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -7,7 +7,7 @@ return {
       dependencies = { "nvim-neotest/nvim-nio" },
       -- stylua: ignore
       keys = {
-        { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+        { "<leader>du", function() require("dapui").toggle() end, silent = true, desc = "Dap UI" },
         { "<leader>dE", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
       },
       opts = {},
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -41,33 +41,47 @@ return {
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   { "L3MON4D3/LuaSnip", enabled = false },
 
-  {
+  { -- override nvim-autopairs plugin
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
+      -- run default AstroNvim config
+      require("plugins.configs.nvim-autopairs")(plugin, opts)
+      -- require Rule function
+      local Rule = require("nvim-autopairs.rule")
+      local npairs = require("nvim-autopairs")
+      npairs.add_rules({
         {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          -- specify a list of rules to add
+          Rule(" ", " "):with_pair(function(options)
+            local pair = options.line:sub(options.col - 1, options.col)
+            return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+          end),
+          Rule("( ", " )")
+            :with_pair(function()
+              return false
+            end)
+            :with_move(function(options)
+              return options.prev_char:match(".%)") ~= nil
+            end)
+            :use_key(")"),
+          Rule("{ ", " }")
+            :with_pair(function()
+              return false
+            end)
+            :with_move(function(options)
+              return options.prev_char:match(".%}") ~= nil
+            end)
+            :use_key("}"),
+          Rule("[ ", " ]")
+            :with_pair(function()
+              return false
+            end)
+            :with_move(function(options)
+              return options.prev_char:match(".%]") ~= nil
+            end)
+            :use_key("]"),
         },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
+      })
     end,
   },
 
