afc7adae32eeb6f6cc38a9e65f30523200e4e5a1
Ronald Record <github@ronrecord.com>
2024-04-03 22:00:51 -0700

    Use configuration.lua setting for treesitter ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,8 @@
--- Customize Treesitter
-
 local settings = require("configuration")
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, settings.treesitter_ensure_installed)
+    opts.ensure_installed = settings.treesitter_ensure_installed
   end,
 }
