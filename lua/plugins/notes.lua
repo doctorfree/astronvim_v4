7e1e840b5f12f322bfe9fe2788798caa13ab90e9
Ronald Record <github@ronrecord.com>
2024-04-04 10:57:40 -0700

    Fix Neorg build using Luarocks

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -34,7 +34,6 @@ return {
     "nvim-neorg/neorg",
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
-    build = ":Neorg sync-parsers",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       {
