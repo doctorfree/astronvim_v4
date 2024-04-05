9c99c63af3622fb8937b613ac05168db30a72b02
Ronald Record <github@ronrecord.com>
2024-04-05 07:23:17 -0700

    Add test scripts and lazyman install script

---
+++ scripts/inst.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Usage: ./inst.sh [-r] [-t]
+# Where: -r indicates removal, -t indicates perform a test invocation
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+have_real=$(type -p realpath)
+[ "${have_real}" ] && SCRIPT_PATH="$(realpath $SCRIPT_PATH)"
+cd $(dirname "${SCRIPT_PATH}")
+cd ..
+
+CFG="nvim-AstroNvimV4"
+CFDIR="${HOME}/.config/${CFG}"
+
+clean_data() {
+  rm -rf "${HOME}/.local/share/${CFG}"
+  rm -rf "${HOME}/.local/state/${CFG}"
+  rm -rf "${HOME}/.cache/${CFG}"
+}
+
+have_lman=$(type -p lazyman)
+if [ "${have_lman}" ]; then
+  lazyman -R -N ${CFG} -y
+else
+  clean_data
+fi
+rm -rf ${CFDIR}
+if [ "$1" == "-r" ]; then
+  clean_data
+else
+  cp -a astronvim_v4 ${CFDIR}
+  [ "$1" == "-t" ] && {
+    vim ${CFDIR}/lua/configuration.lua
+    export NVIM_APPNAME="${CFG}"
+    nvim ${CFDIR}/lua/configuration.lua
+  }
+fi
---
+++ scripts/inst_template.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+have_real=$(type -p realpath)
+[ "${have_real}" ] && SCRIPT_PATH="$(realpath $SCRIPT_PATH)"
+cd $(dirname "${SCRIPT_PATH}")
+cd ..
+
+CFG="nvim-Template"
+CFDIR="${HOME}/.config/${CFG}"
+V4DIR="astronvim-template"
+
+clean_data() {
+  rm -rf "${HOME}/.local/share/${CFG}"
+  rm -rf "${HOME}/.local/state/${CFG}"
+  rm -rf "${HOME}/.cache/${CFG}"
+}
+
+have_lman=$(type -p lazyman)
+if [ "${have_lman}" ]; then
+  lazyman -R -N ${CFG} -y
+else
+  clean_data
+fi
+rm -rf ${CFDIR}
+if [ "$1" == "-r" ]; then
+  clean_data
+else
+  [ -d ${V4DIR} ] || {
+    git clone https://github.com/AstroNvim/template ${V4DIR}
+  }
+  cp -a ${V4DIR} ${CFDIR}
+  export NVIM_APPNAME="${CFG}"
+  nvim ${CFDIR}/lua/plugins/treesitter.lua
+fi
