640b12f540ee7f6445850abd3b48f73d48361507
Ronald Record <github@ronrecord.com>
2024-04-04 17:47:01 -0700

    Add example mapping in astrocore for Telescope theme

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -55,6 +55,8 @@ return {
         ["<Leader>b"] = { desc = "Buffers" },
         -- quick save
         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        -- Telescope themes
+        ["<Leader>T"] = { "<cmd>Telescope themes<CR>", desc = "Theme switcher" },
       },
       t = {
         -- setting a mapping to false will disable it
