2a8b7a1f15335aa5c972a173b88f717d070378c0
Ronald Record <github@ronrecord.com>
2024-04-06 17:58:50 -0700

    Move yamlls and ccls lsp config to astrolsp

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -255,6 +255,53 @@ return {
         end
       end,
 
+      yamlls = function(_, opts)
+        if table_contains(lsp_all, "yamlls") then
+          lspconfig.yamlls.setup({
+            capabilities = capabilities,
+            schemaStore = {
+              enable = true,
+              url = "https://www.schemastore.org/api/json/catalog.json",
+            },
+            schemas = {
+              kubernetes = "*.yaml",
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+              ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+              ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+              ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+              ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+              ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+              ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+              ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+            },
+            format = { enabled = false },
+            validate = false,
+            completion = true,
+            hover = true,
+          })
+        end
+      end,
+
+      ccls = function(_, opts)
+        if fn.executable("ccls") == 1 then
+          lspconfig.ccls.setup({
+            capabilities = capabilities,
+            init_options = {
+              cache = {
+                directory = ".ccls-cache",
+              },
+              highlight = {
+                lsRanges = true,
+              },
+            },
+          })
+        end
+      end,
+
       clangd = function(_, opts)
         if fn.executable("clangd") == 1 then
           require("lspconfig").clangd.setup({
