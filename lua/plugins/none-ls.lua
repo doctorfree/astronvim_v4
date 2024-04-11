ea36f5d001ce814bfad7357dd4067d006134591f
Ronald Record <github@ronrecord.com>
2024-04-11 11:43:01 -0700

    Fix none-ls setup

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -21,16 +21,16 @@ return {
     local diagnostics = null_ls.builtins.diagnostics
     local actions = null_ls.builtins.code_actions
 
-    local conf_sources = {
+    config.sources = {
       actions.gitsigns,
       diagnostics.zsh.with({ filetypes = { "zsh" } }),
     }
     if table_contains(formatters_linters, "actionlint") then
-      table.insert(conf_sources, diagnostics.actionlint)
+      table.insert(config.sources, diagnostics.actionlint)
     end
     if table_contains(formatters_linters, "stylua") then
       table.insert(
-        conf_sources,
+        config.sources,
         formatting.stylua.with({
           timeout = 10000,
           extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
@@ -39,7 +39,7 @@ return {
     end
     if table_contains(formatters_linters, "prettier") then
       table.insert(
-        conf_sources,
+        config.sources,
         formatting.prettier.with({
           -- milliseconds
           timeout = 10000,
@@ -47,18 +47,18 @@ return {
         })
       )
     else
-      table.insert(conf_sources, formatting.eslint_d)
+      table.insert(config.sources, formatting.eslint_d)
     end
     if table_contains(external_formatters, "ruff") then
       if vim.fn.executable("ruff") == 1 then
-        table.insert(conf_sources,
+        table.insert(config.sources,
           require("none-ls.diagnostics.ruff"))
       end
     end
     if table_contains(external_formatters, "black") then
       if vim.fn.executable("black") == 1 then
         table.insert(
-          conf_sources,
+          config.sources,
           formatting.black.with({
             timeout = 10000,
             extra_args = { "--fast" },
@@ -69,7 +69,7 @@ return {
     if table_contains(external_formatters, "beautysh") then
       if vim.fn.executable("beautysh") == 1 then
         table.insert(
-          conf_sources,
+          config.sources,
           require("none-ls.formatting.beautysh").with({
             timeout = 10000,
             extra_args = { "--indent-size", "2" },
@@ -80,7 +80,7 @@ return {
     if table_contains(external_formatters, "flake8") then
       if vim.fn.executable("flake8") == 1 then
         table.insert(
-          conf_sources,
+          config.sources,
           require("none-ls.diagnostics.flake8").with({
             timeout = 10000,
             extra_args = {
@@ -94,7 +94,7 @@ return {
     end
     if table_contains(formatters_linters, "latexindent") then
       table.insert(
-        conf_sources,
+        config.sources,
         -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
         formatting.latexindent.with({
           timeout = 10000,
@@ -104,7 +104,7 @@ return {
     end
     if table_contains(formatters_linters, "shellcheck") then
       table.insert(
-        conf_sources,
+        config.sources,
         actions.shellcheck.with({
           timeout = 10000,
           filetypes = { "sh", "zsh", "bash" },
@@ -113,7 +113,7 @@ return {
     end
     if table_contains(formatters_linters, "shfmt") then
       table.insert(
-        conf_sources,
+        config.sources,
         formatting.shfmt.with({
           timeout = 10000,
           extra_args = { "-i", "2", "-ci", "-bn" },
@@ -123,7 +123,7 @@ return {
     end
     if table_contains(formatters_linters, "sql_formatter") then
       table.insert(
-        conf_sources,
+        config.sources,
         formatting.sql_formatter.with({
           timeout = 10000,
           extra_args = { "--config" },
@@ -132,7 +132,7 @@ return {
     end
     if table_contains(formatters_linters, "markdownlint") then
       table.insert(
-        conf_sources,
+        config.sources,
         formatting.markdownlint.with({
           timeout = 10000,
         })
@@ -141,7 +141,7 @@ return {
     if settings.enable_coding then
       if table_contains(formatters_linters, "goimports") then
         table.insert(
-          conf_sources,
+          config.sources,
           formatting.goimports.with({
             timeout = 10000,
           })
@@ -149,7 +149,7 @@ return {
       end
       if table_contains(formatters_linters, "gofumpt") then
         table.insert(
-          conf_sources,
+          config.sources,
           formatting.gofumpt.with({
             timeout = 10000,
           })
@@ -157,7 +157,7 @@ return {
       end
       if table_contains(formatters_linters, "golines") then
         table.insert(
-          conf_sources,
+          config.sources,
           formatting.golines.with({
             timeout = 10000,
           })
@@ -165,7 +165,7 @@ return {
       end
       if table_contains(formatters_linters, "google-jave-format") then
         table.insert(
-          conf_sources,
+          config.sources,
           formatting.google_java_format.with({
             timeout = 10000,
           })
@@ -173,7 +173,6 @@ return {
       end
     end
     
-    -- return final config table
-    return conf_sources
-  end,
+    return config
+  end
 }
